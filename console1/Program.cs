e44627082913a79501b8726cd0fa46b3ca07c81c
Stephan Raab <sarcartist@gmail.com>
2024-12-06 20:02:29 -0500

    classic fizzbuzz

--- console1/Program.cs
+++ console1/Program.cs
@@ -1,3 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿Console.WriteLine("How many times do you want to play fizzbuzz?");
+int max = Convert.ToInt32(Console.ReadLine());
+int fizzbuzzCount = 0;
+int fizzCount = 0;
+int buzzCount = 0;
 
-Console.WriteLine("Hello, World!");
+for (int i = 1; i <= max; i++)
+{
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+        Console.WriteLine("FizzBuzz");
+        fizzbuzzCount++;
+    } else if (i % 3 == 0)
+    {
+        Console.WriteLine("Fizz");
+        fizzCount++;
+    }
+    else
+    {
+        Console.WriteLine("Buzz");
+        buzzCount++;
+    }
+}
+
+Console.WriteLine($"FizzBuzz Count: {fizzbuzzCount}");
+Console.WriteLine($"Fizz Count: {fizzCount}");
+Console.WriteLine($"Buzz Count: {buzzCount}");
+
+
+
