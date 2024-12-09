ffc9e27b26adf61b166187d8fc0be423b9d7731d
Stephan Raab <sarcartist@gmail.com>
2024-12-09 13:07:19 -0500

    car setup

--- console1/Composition/Car.cs
+++ console1/Composition/Car.cs
@@ -2,5 +2,18 @@
 
 public class Car
 {
-    
+    private Chassis _chassis = new Chassis();
+    private Engine _engine = new Engine();
+    private Seats _seats = new Seats();
+    private Wheels _wheels = new Wheels();
+
+    public void StartCar()
+    {
+        _chassis.Support();
+        _engine.Start();
+        _seats.Sit();
+        _wheels.Rotate();
+        
+        Console.WriteLine("Car started");
+    }
 }
