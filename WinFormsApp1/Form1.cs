5905e7468dc1ada4cf5c9cb8fbe7273ce8e7ef1b
Stephan Raab <sarcartist@gmail.com>
2024-12-17 20:55:22 -0500

    seed some bank account data into the grid

--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -5,5 +5,30 @@ public partial class Form1 : Form
     public Form1()
     {
         InitializeComponent();
+        
+        BankAccount bankAccount = new BankAccount
+        {
+            Balance = 1000,
+            BankAccountNumber = Guid.NewGuid(),
+            Owner = "John Doe"
+        };
+        
+        BankAccount bankAccount2 = new BankAccount
+        {
+            Balance = 10000,
+            BankAccountNumber = Guid.NewGuid(),
+            Owner = "Jane Doe"
+        };
+        
+        BankAccount bankAccount3 = new BankAccount
+        {
+            Balance = 10000000,
+            BankAccountNumber = Guid.NewGuid(),
+            Owner = "Bill Gates"
+        };
+        
+        List<BankAccount> bankAccounts = [bankAccount, bankAccount2, bankAccount3];
+        
+        BankAccountsGrid.DataSource = bankAccounts;
     }
 }
