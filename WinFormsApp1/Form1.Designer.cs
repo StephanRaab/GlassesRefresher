a82ba5e65cd218f9eda17275f87d66005c564af6
Stephan Raab <sarcartist@gmail.com>
2024-12-17 21:54:06 -0500

    Had to switch over to visual studio

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -29,92 +29,95 @@ partial class Form1
     /// </summary>
     private void InitializeComponent()
     {
-        label1 = new System.Windows.Forms.Label();
-        label2 = new System.Windows.Forms.Label();
-        OwnerInput = new System.Windows.Forms.TextBox();
-        AmountInput = new System.Windows.Forms.NumericUpDown();
-        BankAccountsGrid = new System.Windows.Forms.DataGridView();
-        DepositBtn = new System.Windows.Forms.Button();
-        WithdrawBtn = new System.Windows.Forms.Button();
-        CreateAccountBtn = new System.Windows.Forms.Button();
+        label1 = new Label();
+        label2 = new Label();
+        OwnerInput = new TextBox();
+        AmountInput = new NumericUpDown();
+        BankAccountsGrid = new DataGridView();
+        DepositBtn = new Button();
+        WithdrawBtn = new Button();
+        CreateAccountBtn = new Button();
         ((System.ComponentModel.ISupportInitialize)AmountInput).BeginInit();
         ((System.ComponentModel.ISupportInitialize)BankAccountsGrid).BeginInit();
         SuspendLayout();
         // 
         // label1
         // 
-        label1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)0));
-        label1.Location = new System.Drawing.Point(14, 17);
+        label1.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+        label1.Location = new Point(14, 17);
         label1.Name = "label1";
-        label1.Size = new System.Drawing.Size(84, 34);
+        label1.Size = new Size(84, 34);
         label1.TabIndex = 0;
         label1.Text = "Owner:";
         // 
         // label2
         // 
-        label2.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)0));
-        label2.Location = new System.Drawing.Point(14, 389);
+        label2.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+        label2.Location = new Point(14, 389);
         label2.Name = "label2";
-        label2.Size = new System.Drawing.Size(96, 34);
+        label2.Size = new Size(96, 34);
         label2.TabIndex = 1;
         label2.Text = "Amount:";
         // 
         // OwnerInput
         // 
-        OwnerInput.Location = new System.Drawing.Point(97, 23);
+        OwnerInput.Location = new Point(97, 23);
         OwnerInput.Name = "OwnerInput";
-        OwnerInput.Size = new System.Drawing.Size(130, 23);
+        OwnerInput.Size = new Size(130, 23);
         OwnerInput.TabIndex = 2;
         // 
         // AmountInput
         // 
-        AmountInput.Location = new System.Drawing.Point(107, 395);
+        AmountInput.Location = new Point(107, 395);
         AmountInput.Name = "AmountInput";
-        AmountInput.Size = new System.Drawing.Size(120, 23);
+        AmountInput.Size = new Size(120, 23);
         AmountInput.TabIndex = 3;
         // 
         // BankAccountsGrid
         // 
-        BankAccountsGrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-        BankAccountsGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-        BankAccountsGrid.Location = new System.Drawing.Point(252, 23);
+        BankAccountsGrid.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+        BankAccountsGrid.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+        BankAccountsGrid.Location = new Point(252, 23);
         BankAccountsGrid.Name = "BankAccountsGrid";
-        BankAccountsGrid.Size = new System.Drawing.Size(531, 336);
+        BankAccountsGrid.Size = new Size(531, 336);
         BankAccountsGrid.TabIndex = 4;
         BankAccountsGrid.Text = "dataGridView1";
         // 
         // DepositBtn
         // 
-        DepositBtn.Location = new System.Drawing.Point(252, 378);
+        DepositBtn.Location = new Point(252, 378);
         DepositBtn.Name = "DepositBtn";
-        DepositBtn.Size = new System.Drawing.Size(257, 40);
+        DepositBtn.Size = new Size(257, 40);
         DepositBtn.TabIndex = 5;
         DepositBtn.Text = "Deposit";
         DepositBtn.UseVisualStyleBackColor = true;
+        DepositBtn.Click += DepositBtn_Click;
         // 
         // WithdrawBtn
         // 
-        WithdrawBtn.Location = new System.Drawing.Point(515, 378);
+        WithdrawBtn.Location = new Point(515, 378);
         WithdrawBtn.Name = "WithdrawBtn";
-        WithdrawBtn.Size = new System.Drawing.Size(268, 40);
+        WithdrawBtn.Size = new Size(268, 40);
         WithdrawBtn.TabIndex = 6;
         WithdrawBtn.Text = "Withdraw";
         WithdrawBtn.UseVisualStyleBackColor = true;
+        WithdrawBtn.Click += WithdrawBtn_Click;
         // 
         // CreateAccountBtn
         // 
-        CreateAccountBtn.Location = new System.Drawing.Point(97, 63);
+        CreateAccountBtn.Location = new Point(97, 63);
         CreateAccountBtn.Name = "CreateAccountBtn";
-        CreateAccountBtn.Size = new System.Drawing.Size(130, 40);
+        CreateAccountBtn.Size = new Size(130, 40);
         CreateAccountBtn.TabIndex = 7;
         CreateAccountBtn.Text = "Create Account\r\n";
         CreateAccountBtn.UseVisualStyleBackColor = true;
+        CreateAccountBtn.Click += CreateAccountBtn_Click;
         // 
         // Form1
         // 
-        AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-        AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        ClientSize = new System.Drawing.Size(800, 450);
+        AutoScaleDimensions = new SizeF(7F, 15F);
+        AutoScaleMode = AutoScaleMode.Font;
+        ClientSize = new Size(800, 450);
         Controls.Add(CreateAccountBtn);
         Controls.Add(WithdrawBtn);
         Controls.Add(DepositBtn);
@@ -123,6 +126,7 @@ partial class Form1
         Controls.Add(OwnerInput);
         Controls.Add(label2);
         Controls.Add(label1);
+        Name = "Form1";
         Text = "Form1";
         ((System.ComponentModel.ISupportInitialize)AmountInput).EndInit();
         ((System.ComponentModel.ISupportInitialize)BankAccountsGrid).EndInit();
--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -2,16 +2,34 @@ namespace WinFormsApp1;
 
 public partial class Form1 : Form
 {
+    List<BankAccount> BankAccounts = new List<BankAccount>();
+
     public Form1()
     {
         InitializeComponent();
+    }
 
-        BankAccount bankAccount = new BankAccount("Jeffrei Besos");
-        BankAccount bankAccount2 = new BankAccount("Marky Zucker");
-        BankAccount bankAccount3 = new BankAccount("Billiam Gates");
-        
-        List<BankAccount> bankAccounts = [bankAccount, bankAccount2, bankAccount3];
-        
-        BankAccountsGrid.DataSource = bankAccounts;
+    private void CreateAccountBtn_Click(object sender, EventArgs e)
+    {
+        BankAccount bankAccount = new BankAccount(OwnerInput.Text);
+        BankAccounts.Add(bankAccount);
+
+        RefreshGrid();
+    }
+
+    private void DepositBtn_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    private void WithdrawBtn_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    private void RefreshGrid()
+    {
+        BankAccountsGrid.DataSource = null;
+        BankAccountsGrid.DataSource = BankAccounts;
     }
 }
