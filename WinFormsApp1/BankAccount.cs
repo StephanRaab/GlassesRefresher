12f57bc6a1ab1c5821c7f0d65f592e8d5ae0ea29
Stephan Raab <sarcartist@gmail.com>
2024-12-17 21:12:24 -0500

    small refactor

--- WinFormsApp1/BankAccount.cs
+++ WinFormsApp1/BankAccount.cs
@@ -5,4 +5,26 @@ public class BankAccount
     public string Owner { get; set; }
     public Guid BankAccountNumber { get; set; }
     public decimal Balance { get; set; }
+
+    public void CreateBankAccount()
+    {
+        
+    }
+
+    public void Deposit(decimal amount, string owner, string bankAccountNumber)
+    {
+        
+    }
+
+    public void Withdraw(decimal amount, string owner, string bankAccountNumber)
+    {
+        
+    }
+    
+    public BankAccount(string owner)
+    {
+        Owner = owner;
+        BankAccountNumber = Guid.NewGuid();
+        Balance = 0;
+    }
 }
--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -5,27 +5,10 @@ public partial class Form1 : Form
     public Form1()
     {
         InitializeComponent();
-        
-        BankAccount bankAccount = new BankAccount
-        {
-            Balance = 1000,
-            BankAccountNumber = Guid.NewGuid(),
-            Owner = "John Doe"
-        };
-        
-        BankAccount bankAccount2 = new BankAccount
-        {
-            Balance = 10000,
-            BankAccountNumber = Guid.NewGuid(),
-            Owner = "Jane Doe"
-        };
-        
-        BankAccount bankAccount3 = new BankAccount
-        {
-            Balance = 10000000,
-            BankAccountNumber = Guid.NewGuid(),
-            Owner = "Bill Gates"
-        };
+
+        BankAccount bankAccount = new BankAccount("Jeffrei Besos");
+        BankAccount bankAccount2 = new BankAccount("Marky Zucker");
+        BankAccount bankAccount3 = new BankAccount("Billiam Gates");
         
         List<BankAccount> bankAccounts = [bankAccount, bankAccount2, bankAccount3];
         
