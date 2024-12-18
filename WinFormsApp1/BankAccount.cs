725f878a6287e4b10949d701b3c5ee85e916c8d1
Stephan Raab <sarcartist@gmail.com>
2024-12-17 20:49:01 -0500

    start adding BankAccount class

---
+++ WinFormsApp1/BankAccount.cs
@@ -0,0 +1,8 @@
+﻿namespace WinFormsApp1;
+
+public class BankAccount
+{
+    public string Owner { get; set; }
+    public Guid BankAccountNumber { get; set; }
+    public decimal Balance { get; set; }
+}
